ee8deb5929f4f1527879dadec6155606da54a1e4
kimdal <kimdal1029@naver.com>
2021-11-03 18:10:04 +0900

    move function setup -> draw

--- sketch.js
+++ sketch.js
@@ -2,13 +2,16 @@ let particles = [];
 
 function setup() {
   createCanvas(400, 400);
-  for(let i=0; i<100; i++){
-  particles.push(new Particle(200, 200));
-}
+//   for(let i=0; i<100; i++){
+//   particles.push(new Particle(200, 200));
+// }
 }
 
 function draw() {
   background(0);
+  for(let i=0; i<5; i++){
+  particles.push(new Particle(200, 200));
+}
 for (let particle of particles){
   let gravity = createVector(0, 0.2);
   particle.applyForce(gravity);
