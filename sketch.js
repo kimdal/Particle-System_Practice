a72fede6f99f35ea696b52a968332c6707e52cba
kimdal <kimdal1029@naver.com>
2021-11-03 20:44:46 +0900

    update mousePressed

--- sketch.js
+++ sketch.js
@@ -2,10 +2,10 @@
 let emitters = [];
 function setup() {
   createCanvas(400, 400);
-//   for(let i=0; i<500; i++){
-//   particles.push(new Particle(200, 200));
-// }
-emitters[0] = new Emitter(200, 20);
+}
+
+function mousePressed(){
+  emitters.push(new Emitter(mouseX, mouseY));
 }
 
 function draw() {
