e16483c437041c0a7ebc7042fe2bf13fb5f07a5d
kimdal <kimdal1029@naver.com>
2021-11-03 20:12:29 +0900

    add this.

--- emitter.js
+++ emitter.js
@@ -4,21 +4,21 @@ class Emitter{
     this.particles = [];
   }
 update(){
-  for (let particle of particles){
+  for (let particle of this.particles){
   let gravity = createVector(0, 0.2);
-  particle.applyForce(gravity);
+  thiparticle.applyForce(gravity);
   particle.update();
 }
-  for (let i = particles.length-1; i>=0; i--){
-    if(particles[i].finished()){
-      particles.splice(i, 1);
+  for (let i = this.particles.length-1; i>=0; i--){
+    if(this.particles[i].finished()){
+      this.particles.splice(i, 1);
     }
   }
 }
 
 show(){
-  for (let particle of particles){
-    particle.show();
+  for (let particle of this.particles){
+    this.particle.show();
     //particle.edges();
   }
 }
