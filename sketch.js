16618fc64ee0420626aef3c4846bcbc0a2fa78b9
kimdal <kimdal1029@naver.com>
2021-11-03 20:11:59 +0900

    move function to emitter class

--- sketch.js
+++ sketch.js
@@ -1,28 +1,31 @@
-let particles = [];
-
+//let particles = [];
+let emitter;
 function setup() {
   createCanvas(400, 400);
 //   for(let i=0; i<500; i++){
 //   particles.push(new Particle(200, 200));
 // }
+let emitter = new Emitter(200, 20);
 }
 
 function draw() {
   background(0);
-  for(let i=0; i<5; i++){
-  particles.push(new Particle(200, 20));
-}
-for (let particle of particles){
-  let gravity = createVector(0, 0.2);
-  particle.applyForce(gravity);
-  particle.update();
-  particle.show();
-  //particle.edges();
-}
-
-for (let i = particles.length-1; i>=0; i--){
-  if(particles[i].finished()){
-    particles.splice(i, 1);
-  }
-}
+//   for(let i=0; i<5; i++){
+//   particles.push(new Particle(200, 20));
+// }
+// for (let particle of particles){
+//   let gravity = createVector(0, 0.2);
+//   particle.applyForce(gravity);
+//   particle.update();
+//   particle.show();
+//   //particle.edges();
+// }
+//
+// for (let i = particles.length-1; i>=0; i--){
+//   if(particles[i].finished()){
+//     particles.splice(i, 1);
+//   }
+// }
+emitter.show();
+emitter.update();
 }
