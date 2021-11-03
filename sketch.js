ff7ef1e03612b1c5b9e7a1bc3bf08c7fca736ad6
kimdal <kimdal1029@naver.com>
2021-11-03 18:32:56 +0900

    modify 'particle' position

--- sketch.js
+++ sketch.js
@@ -10,14 +10,14 @@ function setup() {
 function draw() {
   background(0);
   for(let i=0; i<5; i++){
-  particles.push(new Particle(200, 200));
+  particles.push(new Particle(200, 20));
 }
 for (let particle of particles){
   let gravity = createVector(0, 0.2);
   particle.applyForce(gravity);
   particle.update();
   particle.show();
-  particle.edges();
+  //particle.edges();
 }
 
 for (let i = particles.length-1; i>=0; i--){
