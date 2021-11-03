5b8e20687fb1a9917a63dc497af00102cade6e63
kimdal <kimdal1029@naver.com>
2021-11-03 16:52:23 +0900

    add values in calss

--- particle.js
+++ particle.js
@@ -9,4 +9,24 @@ class particle {
   applyForce(force){
     this.acc.add(force);
   }
+
+  update(){
+this.vel.add(this.acc);
+this.pos.add(this.vel);
+this.acc.set(0, 0);
+  }
+
+  show(){
+stroke(255);
+strokeWeight(2);
+fill(255, 100);
+ellipse(this.pos.x, this.pos.y, this.r*2);
+  }
+
+  edges(){
+    if(this.pos.y >= height-this.r){
+      this.pos.y = height-this.r;
+      this.vel.y *= -1;
+    }
+  }
 }
