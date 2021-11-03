611aca7c33e478de93fcfcaefe4935553cfe5901
kimdal <kimdal1029@naver.com>
2021-11-03 17:55:02 +0900

    modify class to array

--- sketch.js
+++ sketch.js
@@ -1,16 +1,19 @@
-let particle;
+let particles = [];
 
 function setup() {
   createCanvas(400, 400);
-  particle = new Particle(200, 200);
+  for(let i=0; i<100; i++){
+  particles.push(new Particle(200, 200));
+}
 }
 
 function draw() {
   background(0);
-
+for (let particle of particles){
   let gravity = createVector(0, 0.2);
   particle.applyForce(gravity);
   particle.update();
   particle.show();
   particle.edges();
+}
 }
